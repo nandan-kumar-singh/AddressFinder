af6ad4c09f332196e925af10af260dee3af22118
nandan-kumar-singh <nandansingh024@gmail.com>
2017-05-15 19:29:13 +0530

    Update AddressFinder.java

--- AddressFinder.java
+++ AddressFinder.java
@@ -12,7 +12,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
-
+import java.io.Serializable;
 import in.androidfluid.locationsearch.model.Address;
 
 /**
@@ -151,4 +151,126 @@ public abstract class AddressFinder extends AsyncTask<Void, Void, Address> {
     }
 
     abstract void onGetAddress(Address address);
+}
+
+
+
+ class Address {
+
+    String Address1 = "";
+    String Address2 = "";
+    String City = "";
+    String State = "";
+    String Country = "";
+    String County = "";
+    String PIN = "";
+    String Area = "";
+    private String address;
+    private double latitude;
+    private double longitude;
+
+    public String getAddress1() {
+        return Address1;
+    }
+
+    public void setAddress1(String address1) {
+        Address1 = address1;
+    }
+
+    public String getAddress2() {
+        return Address2;
+    }
+
+    public void setAddress2(String address2) {
+        Address2 = address2;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String city) {
+        City = city;
+    }
+
+    public String getState() {
+        return State;
+    }
+
+    public void setState(String state) {
+        State = state;
+    }
+
+    public String getCountry() {
+        return Country;
+    }
+
+    public void setCountry(String country) {
+        Country = country;
+    }
+
+    public String getCounty() {
+        return County;
+    }
+
+    public void setCounty(String county) {
+        County = county;
+    }
+
+    public String getPIN() {
+        return PIN;
+    }
+
+    public void setPIN(String PIN) {
+        this.PIN = PIN;
+    }
+
+    public String getArea() {
+        return Area;
+    }
+
+    public void setArea(String area) {
+        Area = area;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "address='" + address + '\'' +
+                ", latitude=" + latitude +
+                ", longitude=" + longitude +
+                ", Address1='" + Address1 + '\'' +
+                ", Address2='" + Address2 + '\'' +
+                ", City='" + City + '\'' +
+                ", State='" + State + '\'' +
+                ", Country='" + Country + '\'' +
+                ", County='" + County + '\'' +
+                ", PIN='" + PIN + '\'' +
+                ", Area='" + Area + '\'' +
+                '}';
+    }
 }
